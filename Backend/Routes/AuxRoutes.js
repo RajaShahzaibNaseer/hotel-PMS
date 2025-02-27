df7d2c9cb60cf1d5ef9569c77a624284ad67c60e
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-02-27 15:48:48 +0500

    made services working

--- Backend/Routes/AuxRoutes.js
+++ Backend/Routes/AuxRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const BaseController = require("../Controllers/BaseController");
 
 const router = express.Router();
-const auxController = new BaseController("agents");
+const auxController = new BaseController("services");
 
 router.get("/", auxController.getAll.bind(auxController));
 router.get("/:id", auxController.getOne.bind(auxController));

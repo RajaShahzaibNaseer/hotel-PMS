8b0854b97b701084f9d5d95bbe0a7f90d6e4facb
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-02-27 15:49:14 +0500

    made company working

--- Backend/Routes/CompanyRoutes.js
+++ Backend/Routes/CompanyRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const BaseController = require("../Controllers/BaseController");
 
 const router = express.Router();
-const companyController = new BaseController("agents");
+const companyController = new BaseController("company");
 
 router.get("/", companyController.getAll.bind(companyController));
 router.get("/:id", companyController.getOne.bind(companyController));

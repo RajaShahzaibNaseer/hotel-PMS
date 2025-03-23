585b4be6e7ac47fc613110ccbfc85f9b1b161fc4
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-03-23 14:34:57 +0500

    Added backend routes for category

---
+++ Backend/Routes/CategoryRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const BaseController = require("../Controllers/BaseController");
+
+const router = express.Router();
+const categoryController = new BaseController("category");
+
+router.get("/", categoryController.getAll.bind(categoryController));
+router.get("/:id", categoryController.getOne.bind(categoryController));
+router.post("/", categoryController.create.bind(categoryController));
+router.put("/:id", categoryController.update.bind(categoryController));
+router.delete("/:id", categoryController.delete.bind(categoryController));
+
+module.exports = router;
+
